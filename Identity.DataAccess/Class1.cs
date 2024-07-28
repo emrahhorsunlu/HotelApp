ff8efea23e5fcda98c2adbb8f832668e5839b051
emrahhorsunlu <73930484+emrahhorsunlu@users.noreply.github.com>
2024-07-28 21:54:25 +0300

    Identity Service Remove

--- Identity.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Identity.DataAccess
-{
-    public class Class1
-    {
-    }
-}
--- Identity.Entities/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Identity.Entities
-{
-    public class Class1
-    {
-    }
-}
